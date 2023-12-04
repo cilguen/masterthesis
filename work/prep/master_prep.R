54394170289a361bfa06db00bb3df316583286db
cilguen <152862313+cilguen@users.noreply.github.com>
2023-12-04 20:06:43 +0100

    new file structure
    reordered some files and scripts

---
+++ work/prep/master_prep.R
@@ -0,0 +1,13 @@
+# master script
+
+# load answers
+source("C:/Users/Can/Desktop/masterthesis/masterthesis/work/prep/load_answers.R")
+
+# load demographics
+source("C:/Users/Can/Desktop/masterthesis/masterthesis/work/prep/load_demographics.R")
+
+# merge answers and demographics
+source("C:/Users/Can/Desktop/masterthesis/masterthesis/work/prep/merge_data.R")
+
+# clean dataset
+source("C:/Users/Can/Desktop/masterthesis/masterthesis/work/prep/clean_data.R")
