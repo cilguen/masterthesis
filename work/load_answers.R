e19927d7cd8a4413c7dc5ed7cbd9f206d360ca40
cilguen <152862313+cilguen@users.noreply.github.com>
2023-12-04 20:00:22 +0100

    prep
    finished data prep

--- work/load_answers.R
+++ work/load_answers.R
@@ -1,7 +1,7 @@
 # This is code to load the research data from Prolific into R
 
 # Replace the file path with your actual file path
-answers_file_path <- "C:\\Users\\Can\\Desktop\\masterthesis\\masterthesis\\data\\UG.csv"
+answers_file_path <- "C:/Users/Can/Desktop/masterthesis/masterthesis/data/UG.csv"
 
 # Read the CSV file into a data frame
 answers <- read.csv(answers_file_path)
---
+++ work/master_prep.R
@@ -0,0 +1,13 @@
+# master script
+
+# load answers
+source("C:/Users/Can/Desktop/masterthesis/masterthesis/work/load_answers.R")
+
+# load demographics
+source("C:/Users/Can/Desktop/masterthesis/masterthesis/work/load_demographics.R")
+
+# merge answers and demographics
+source("C:/Users/Can/Desktop/masterthesis/masterthesis/work/merge_data.R")
+
+# clean dataset
+source("C:/Users/Can/Desktop/masterthesis/masterthesis/work/clean_data.R")
